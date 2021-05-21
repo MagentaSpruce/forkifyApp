7943377749b439cbbeeea285529c15b3302b9019
MagentaSpruce <magentaspruce@protonmail.com>
2021-05-21 13:09:17 -0400

    Started implementing the upload new recipe functionality. Started on the AddRecipeView class and methods.

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,6 +5,7 @@ import searchView from './views/searchView';
 import resultsView from './views/resultsView';
 import paginationView from './views/paginationView';
 import bookmarksView from './views/bookmarksView';
+import addRecipeView from './views/addRecipeView';
 
 //For polyfilling all except Async/Await
 import 'core-js/stable';
@@ -98,6 +99,12 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+
+  //Upload the new recipe data
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -105,5 +112,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
