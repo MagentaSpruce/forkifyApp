d169ccc6427c023868ec9a81ea65adfd520e8005
MagentaSpruce <magentaspruce@protonmail.com>
2021-05-20 21:17:03 -0400

    Instituted an updating algorithm to update the DOM

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,6 +25,8 @@ const controlRecipes = async function () {
 
     if (!id) return;
     recipeView.renderSpinner();
+    //0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
     //1) Loading recipe - waiting for data from the model. loadRecipe is async, so must await.
     //.loadRecipe(id) gives access to state.recipe
     await model.loadRecipe(id);
@@ -70,7 +72,8 @@ const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
